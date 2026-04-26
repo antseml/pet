2ef1cb7d1a54b372bba0ca29f8486c8af79d9d2e
antseml <semensemen123123ee@gmail.com>
2026-04-26 22:11:36 +0300

    Delete study-site-backup/s-site directory

--- study-site-backup/s-site/app/database.py
+++
@@ -1,7 +0,0 @@
-﻿"""Конфигурация и единая точка доступа к экземпляру SQLAlchemy."""
-
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-# Экземпляр используется во всех модулях приложения через импорт `db`.
